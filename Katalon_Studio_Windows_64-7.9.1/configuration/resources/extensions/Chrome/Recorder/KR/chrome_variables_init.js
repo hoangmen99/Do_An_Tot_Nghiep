f14220d17459b49d5a0fcb558abaaa2b31ab8e1f
Bảo <bao.nguyen2@sotatek.com>
2021-06-15 21:25:16 +0700

    15/6

--- Katalon_Studio_Windows_64-7.9.1/configuration/resources/extensions/Chrome/Recorder/KR/chrome_variables_init.js
+++ Katalon_Studio_Windows_64-7.9.1/configuration/resources/extensions/Chrome/Recorder/KR/chrome_variables_init.js
@@ -1,4 +1,4 @@
-katalonServerPort = '54114'
+katalonServerPort = '65013'
 katalonOnOffStatus = true
 spy_captureObjectHotKey = {"keyCode":192,"useAltKey":true,"useShiftKey":false,"useCtrlKey":false,"useMetaKey":false};
 spy_loadDomMapHotKey = {"keyCode":192,"useAltKey":true,"useShiftKey":false,"useCtrlKey":true,"useMetaKey":false};
